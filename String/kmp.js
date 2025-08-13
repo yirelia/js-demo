3988597f1afc04d1b8ea6fbddd1b83f10b56e2ad
yangrui <17368465776@163.com>
2025-08-13 08:16:05 +0800

    feat: 调整目录

---
+++ String/kmp.js
@@ -0,0 +1,23 @@
+function failFunction(str) {
+    let i = 0;
+    // 第一个失效值总是 0 
+    let failArr = [0]
+    for(let s = 1; s < str.length; s++) {
+        console.log(`s: ${s} i ${i}`)
+        while( i > 0 && str[i] !== str[s] ) {
+            i = failArr[i - 1]
+        }
+        if(str[i] === str[s]) {
+            i++;
+        }
+        failArr[s] = i
+    }
+    return failArr
+}
+
+function kmp() {
+    let 
+}
+const s = 'ababaa'
+let res = failFunction(s)
+console.log(res);
